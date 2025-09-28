02e136ae38400b48cccf7b556676afe2eee34a4a
Mohd Zaid <zaidalig1234@gmail.com>
2025-09-28 06:49:14 +0530

    fourth commit

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+ interface Test {
+    public  void test();
 
-public interface Test {
+
 }
+class Test2 implements Test{
+public void test(){
+    System.out.println("test");
+}
+
+}
+class Main {
+    public static void main(String[] args) {
+        Test2 test = new Test2();
+        test.test();
+        System.out.println("hello i am zaid ");
+    }
+}
+
